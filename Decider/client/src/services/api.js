ea530ec26ce63a9e05edc0457948d3d30edadc5a
Abhijith <connectwithabhijithrs@gmail.com>
2026-03-02 00:13:21 +0530

    connected production backend

--- Decider/client/src/services/api.js
+++ Decider/client/src/services/api.js
@@ -1,5 +1,5 @@
 export async function fetchCriteria(decisionText,count) {
-  const response = await fetch("http://localhost:5000/api/generate", {
+  const response = await fetch("https://decider-xxxx.onrender.com/api/generate", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
