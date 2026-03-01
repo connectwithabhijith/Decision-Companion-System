8aabc3ee6e77f5d8c4cfd01b2ae82d77a9ae1bcd
Abhijith <connectwithabhijithrs@gmail.com>
2026-03-01 22:35:48 +0530

    update server file

--- Decider/server/index.js
+++ Decider/server/index.js
@@ -87,6 +87,7 @@ Do not include markdown.
   }
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
