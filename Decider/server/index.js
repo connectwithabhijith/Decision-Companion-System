2e24ddb4af4ae8c66e652c5371631385862208c7
Abhijith <connectwithabhijithrs@gmail.com>
2026-03-02 00:23:27 +0530

    fixed cors

--- Decider/server/index.js
+++ Decider/server/index.js
@@ -6,7 +6,10 @@ import OpenAI from "openai";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
+
 app.use(express.json());
 
 const groq = new OpenAI({
